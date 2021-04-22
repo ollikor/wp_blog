ca28f3dee2ae09369b99d520c81a70b279bb0b9e
olli <olli.korhonen19@gmail.com>
2021-04-22 12:19:39 +0300

    edit functions.php

--- functions.php
+++ functions.php
@@ -133,4 +133,10 @@ function website_remove($fields)
 }
 add_filter('comment_form_default_fields', 'website_remove');
 
+
+
+function disable_mytheme_action() {
+    define('DISALLOW_FILE_EDIT', TRUE);
+}
+add_action('init', 'disable_mytheme_action');
 ?>
