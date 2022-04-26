e7254383a5f6372e8ccbce7d70ac6c6e2c653969
olli <olli.korhonen19@gmail.com>
2022-04-26 12:46:12 +0300

    add content none template

--- index.php
+++ index.php
@@ -1,25 +1,38 @@
 <?php 
-    get_header();
-?>
+/**
+ * The main template
+ * 
+ * This is the template that displays all posts.
+ * 
+ * @package Wordpress
+ * @subpackage Mytheme
+ * @since Mytheme 1.0
+ * @version 1.0
+ * 
+ */
+
+ get_header(); ?>
 
 <?php
-    if ( has_nav_menu('categories-menu') ) :
-        wp_nav_menu(
-            array(
-                'menu' => 'categories-menu',
-                'container' => '',
-                'theme_location' => 'primary',
-                'items_wrap' => '<ul id="" class="categories-menu">%3$s</ul>',
-            )
-        );
+    if( have_posts() ) :
+        if ( has_nav_menu('categories-menu') ) :
+            wp_nav_menu(
+                array(
+                    'menu' => 'categories-menu',
+                    'container' => '',
+                    'theme_location' => 'primary',
+                    'items_wrap' => '<ul id="" class="categories-menu">%3$s</ul>',
+                )
+            );
+        endif;
     endif;
 ?>
 
 <div class="posts-container">
 
-    <section class="posts-section">
+    <div class="posts-section">
 
-        <article class="posts">
+        <div class="posts">
             
             <?php
 
@@ -33,40 +46,44 @@
 
                     endwhile;
                     
+                else :
+                    get_template_part('template-parts/content', 'none' );
                 endif;
                 ?>
 
-        </article>
+            </div>
 
         <?php 
             the_posts_pagination(); 
         ?>
 
-    </section>
+    </div>
 
-    <aside class="posts-aside">
+    <?php if(have_posts() ) : ?>
+        <div class="posts-aside">
 
-        <div class="sidebar">
-            <h3>Recent Posts</h3>
-            <?php
-            $recent_posts = wp_get_recent_posts();
-            foreach( $recent_posts as $recent ) : ?>
-            <li>
-                <a href="<?php echo esc_url( get_permalink( $recent['ID'] ) ); ?>">
-                    <?php echo esc_html($recent['post_title']); ?><br>
-                    <?php echo esc_html(date('d F Y', strtotime($recent['post_date']))); ?>
-                </a>
-            </li>
-            <?php endforeach; ?>
+            <div class="sidebar">
+                <h3>Recent Posts</h3>
+                <?php
+                $recent_posts = wp_get_recent_posts();
+                foreach( $recent_posts as $recent ) : ?>
+                <li>
+                    <a href="<?php echo esc_url( get_permalink( $recent['ID'] ) ); ?>">
+                        <?php echo esc_html($recent['post_title']); ?><br>
+                        <?php echo esc_html(date('d F Y', strtotime($recent['post_date']))); ?>
+                    </a>
+                </li>
+                <?php endforeach; ?>
+            </div>
+            <div class="sidebar">
+                <h3>Articles</h3>
+                <?php 
+                    wp_get_archives('type=monthly');
+                ?>
+            </div>
+
         </div>
-        <div class="sidebar">
-            <h3>Articles</h3>
-            <?php 
-                wp_get_archives('type=monthly');
-            ?>
-        </div>
-
-    </aside>
+    <?php endif; ?>
 
 </div>
 
---
+++ template-parts/content-none.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+ * 
+ * This is the template what show is there is not posts.
+ * 
+ * @package Wordpress
+ * @subpackage Mytheme
+ * @since Mytheme 1.0
+ * @version 1.0
+ * 
+ */
+ 
+get_header(); 
+?>
+
+<div>
+
+    <h2>No found.</h2>
+
+</div>
+
+<?php
+    get_footer();
+?>
