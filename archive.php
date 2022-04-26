eed5e741c8ce5fac9d5bc17ecc836d1287c661f7
olli <olli.korhonen19@gmail.com>
2022-04-26 13:17:03 +0300

    edit archive template

--- archive.php
+++ archive.php
@@ -1,5 +1,17 @@
 <?php 
-    get_header(); 
+/**
+ * The archive template
+ * 
+ * This is the template that displays posts from categories.
+ * 
+ * @package Wordpress
+ * @subpackage Mytheme
+ * @since Mytheme 1.0
+ * @version 1.0
+ * 
+ */
+
+ get_header(); 
 ?>
 
 <?php
@@ -14,8 +26,8 @@
 ?>
 
 <div class="posts-container">
-    <section class="posts-section">
-        <article class="posts">
+    <div class="posts-section">
+        <div class="posts">
 
         <?php
             if( have_posts() ) :
@@ -24,44 +36,49 @@
 
                     the_post();
                     
-                    echo "sfda";
                     get_template_part( 'template-parts/content', 'archive' );
                 
                 endwhile;
+            
+            else : 
+
+                get_template_part('template-parts/content', 'none' );
 
             endif;
         ?>
 
 
-        </article>
+        </div>
 
         <?php 
             the_posts_pagination();
         ?>
 
-    </section>
+        </div>
 
-    <aside class="posts-aside">
-    <div class="sidebar">
-            <h3>Recent Posts</h3>
-            <?php
-            $recent_posts = wp_get_recent_posts();
-            foreach( $recent_posts as $recent ) : ?>
-            <li>
-                <a href="<?php echo esc_url( get_permalink( $recent['ID'] ) ); ?>">
-                    <?php echo esc_html($recent['post_title']); ?><br>
-                    <?php echo esc_html(date('d F Y', strtotime($recent['post_date']))); ?>
-                </a>
-            </li>
-            <?php endforeach; ?>
+    <?php if( have_posts() ) : ?>
+        <div class="posts-aside">
+            <div class="sidebar">
+                    <h3>Recent Posts</h3>
+                    <?php
+                    $recent_posts = wp_get_recent_posts();
+                    foreach( $recent_posts as $recent ) : ?>
+                        <li>
+                            <a href="<?php echo esc_url( get_permalink( $recent['ID'] ) ); ?>">
+                                <?php echo esc_html($recent['post_title']); ?><br>
+                                <?php echo esc_html(date('d F Y', strtotime($recent['post_date']))); ?>
+                            </a>
+                        </li>
+                    <?php endforeach; ?>
+            </div>
+            <div class="sidebar">
+                <h3>Articles</h3>
+                <?php 
+                    wp_get_archives('type=monthly');
+                ?>
+            </div>
         </div>
-        <div class="sidebar">
-            <h3>Articles</h3>
-            <?php 
-                wp_get_archives('type=monthly');
-            ?>
-        </div>
-    </aside>
+    <?php endif; ?>
 </div>
 
 <?php 
